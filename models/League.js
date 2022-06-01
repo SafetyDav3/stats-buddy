cfb12584f5c81ce8aa7de4fceb495b95b6b0cf45
SafteyDav3 <talktomedw@gmail.com>
2022-06-01 12:35:54 -0500

    Sequelize models

--- models/League.js
+++ models/League.js
@@ -3,12 +3,21 @@ const sequelize = require("../config/connection");
 
 class League extends Model {}
 
-League.init({
-  league_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+League.init(
+  {
+    league_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
   },
-});
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "league",
+  }
+);
 
 module.exports = League;
--- models/Team.js
+++ models/Team.js
@@ -3,12 +3,21 @@ const sequelize = require("../config/connection");
 
 class Team extends Model {}
 
-Team.init({
-  team_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+Team.init(
+  {
+    team_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
   },
-});
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "team",
+  }
+);
 
 module.exports = Team;
