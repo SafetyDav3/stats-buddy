ad67aee102d85d50dc3fb4b514bb66156c3d16d5
Alan Edlebeck <alan.edlebeck@gmail.com>
2022-06-02 20:43:55 -0500

    api routes added

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,18 @@
 const router = require('express').Router();
 
 
+const userRoutes = require('./user-routes.js');
+const gameRoutes = require('./game-routes');
+const leagueRoutes = require('./league-routes');
+const playerRoutes = require('./player-routes');
+const teamRoutes = require('./team-routes');
+
+
+
+router.use('/games', gameRoutes);
+router.use('/leagues', leagueRoutes);
+router.use('/players', playerRoutes);
+router.use('/teams', teamRoutes)
+router.use('/users', userRoutes)
+
 module.exports = router;
