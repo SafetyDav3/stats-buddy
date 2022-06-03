7a73a54693c3df8f069748f983faaa7564db9db0
Hephaestus <mcmahonmike9@yahoo.com>
2022-06-02 19:15:08 -0500

    homeBase routes pushed for input

--- controllers/homeBase-routes.js
+++ controllers/homeBase-routes.js
@@ -5,12 +5,3 @@ const { League, Player, Team } = require('../models');
 router.get('/', async, (req, res) => {
     
 });
-
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
-  });
