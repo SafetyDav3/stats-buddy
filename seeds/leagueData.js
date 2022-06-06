ff6644f420924c7fc5f5b20ae5207ef50e2ab43f
SafteyDav3 <talktomedw@gmail.com>
2022-06-05 21:13:51 -0500

    seed data

--- seeds/leagueData.js
+++ seeds/leagueData.js
@@ -3,7 +3,7 @@ const { League } = require("../models");
 const leagueData = [
   {
     name: "Round Tripper",
-    user_id: "1",
+    user_id: 1,
   },
 ];
 
--- seeds/teamData.js
+++ seeds/teamData.js
@@ -4,32 +4,32 @@ const teamData = [
   {
     team_name: "Micro Brewers",
     manager: "Tony La Russa",
-    league_id: "1",
+    league_id: 1,
   },
   {
     team_name: "108 Stitches",
     manager: "Joe Madded",
-    league_id: "1",
+    league_id: 1,
   },
   {
     team_name: "Catchers In the Rye",
     manager: "Terry Francona",
-    league_id: "1",
+    league_id: 1,
   },
   {
     team_name: "Sultans Of Swing",
     manager: "Dusty Barker",
-    league_id: "1",
+    league_id: 1,
   },
   {
     team_name: "Pancake Batters",
     manager: "Bob Melvin",
-    league_id: "1",
+    league_id: 1,
   },
   {
     team_name: "Sandlot B-Team",
     manager: "Bud Black",
-    league_id: "1",
+    league_id: 1,
   },
 ];
 
