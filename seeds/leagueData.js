e03c8f18fe08ab8b955dfc6b775c668ba901a2eb
Alan Edlebeck <alan.edlebeck@gmail.com>
2022-06-05 20:13:06 -0500

    seed data working

--- seeds/leagueData.js
+++ seeds/leagueData.js
@@ -3,12 +3,10 @@ const { League } = require("../models");
 const leagueData = [
   {
     name: "Round Tripper",
-    user_id: "1",
+    user_id: 1,
   },
 ];
 
-const seedLeague = () => {
-  League.bulkCreate(leagueData);
-};
+const seedLeagues = () => League.bulkCreate(leagueData);
 
-module.exports = seedLeague;
+module.exports = seedLeagues;
