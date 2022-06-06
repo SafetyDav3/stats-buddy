cd42caaf093db64f75bc835ac596d1d7aece3945
Hephaestus <mcmahonmike9@yahoo.com>
2022-06-06 18:26:25 -0500

    pulling new routes

---
+++ controllers/api/game-routes.js
@@ -0,0 +1,3 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { League, Player, Team } = require('../models');
--- controllers/homeBase-routes.js
+++ controllers/homeBase-routes.js
@@ -2,28 +2,4 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { League, Player, Team } = require('../models');
 
-//GET all League
-router.get('/league', async (req, res) => {
-    try {
-      const dbLeagueData = await League.findAll({
-        include: [
-          {
-            model: Team,
-            attributes: ['team_name'],
-          },
-        ],
-      });
-  
-      const leagues = dbLeagueData.map((league) =>
-        league.get({ plain: true })
-      );
-  
-      res.render('homepage', {
-        leagues,
-        loggedIn: req.session.loggedIn,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+
