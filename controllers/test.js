7ef30b3cf86fb201378c2a8bbfdebc665099f6bd
Hephaestus <mcmahonmike9@yahoo.com>
2022-06-08 22:38:46 -0500

    main, home, league, and login handlebars established and tested

--- controllers/test.js
+++ controllers/test.js
@@ -3,7 +3,21 @@ const router = require('express').Router()
 
 
 router.get('/', async (req, res) => {
-    res.render('main')
+    res.render('home')
 })
+
+router.get('/leagues', async (req, res) => {
+    res.render('league')
+})
+
+router.get('/login', async (req, res) => {
+    res.render('login')
+})
+
+// router.get('/', async (req, res) => {
+//     res.render('main')
+// })
+
+
 
 module.exports = router
