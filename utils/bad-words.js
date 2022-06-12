81db5404b1efdb0f183445014e7213493877c646
SafteyDav3 <talktomedw@gmail.com>
2022-06-12 13:11:06 -0500

    bad-words function working

--- utils/bad-words.js
+++ utils/bad-words.js
@@ -1,5 +1,23 @@
-const Filter = require('bad-words');
-const filter = new Filter;
+const Filter = require("bad-words");
+const filter = new Filter();
 
+// const text = "good";
 
-console.log(filter.clean('asshole'));
+console.log(filter.clean(text));
+
+const badWords = function (text) {
+  const checkedText = filter.clean(text);
+  const bleep = /[\*]+/;
+
+  if (bleep.test(checkedText)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// console.log(checkedText);
+// const result = badWords(text);
+// consol.log(result);
+
+module.exports = badWords;
